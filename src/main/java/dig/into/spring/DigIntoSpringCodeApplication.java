52b1d2be0a5e994a5c64dd9dfe3acd66c8215d35
popup0221 <popup0221@naver.com>
2024-07-19 00:09:46 +0900

    first commit

---
+++ src/main/java/dig/into/spring/DigIntoSpringCodeApplication.java
@@ -0,0 +1,13 @@
+package dig.into.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigIntoSpringCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigIntoSpringCodeApplication.class, args);
+	}
+
+}
